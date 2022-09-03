bee847876ae472deb75ad00cd8f0176d7bb4b867
John Grabsky <jgrabsky@hotmail.com>
2022-09-03 16:32:47 -0500

    Initial commit

---
+++ Source/MetalInMotion/MetalInMotion.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MetalInMotion.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MetalInMotion, "MetalInMotion" );
---
+++ Source/MetalInMotion/MetalInMotionGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MetalInMotionGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class METALINMOTION_API AMetalInMotionGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
